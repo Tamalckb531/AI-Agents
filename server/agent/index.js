34c3c1256cd6ba81887f83ad5831c560526cf27e
Tamal <ckbtamaldipnew@gmail.com>
2025-05-31 21:12:00 +0600

    set up agent with server

--- server/agent/index.js
+++ server/agent/index.js
@@ -1,15 +1,33 @@
 import express from 'express'
 import dotenv from "dotenv";
+import cors from "cors";
+import { agent } from './agent.js';
 
 dotenv.config();
-
-
 const app = express();
+
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+
 const port = 3001;
 
 app.get('/', (req, res) => {
     res.send("Hello World!");
 });
+
+app.post('/generate', async (req, res) => {
+    const result = await agent.invoke({
+        messages: [{
+            role: 'user',
+            content: 'Whats the weather in Chittagong ?'
+        }],
+    },
+        {
+            configurable: { thread_id: 42 },
+        });
+
+    res.json(result.messages.at(-1)?.content);
+})
 
 app.listen(port, () => {
     console.log("Server is running on port : ", port);
