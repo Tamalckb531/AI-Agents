1ac99b405424f1eb0434adf73d45bd6f012f28f4
Tamal <ckbtamaldipnew@gmail.com>
2025-06-05 00:24:21 +0600

    created the proper lang graph

--- langgraph-test/index.js
+++ langgraph-test/index.js
@@ -13,7 +13,7 @@ const llm = new ChatGoogleGenerativeAI({
     model: "gemini-1.5-flash",
 });
 
-//! created tools with lang chain -> our llm gonna use that stuffs
+//! created tools with """LangChain""" -> our llm gonna use that stuffs
 const multiply = tool(async(({ a, b }) => {
     return a * b;
 }), {
@@ -62,3 +62,69 @@ const divide = tool(async(({ a, b }) => {
 const tools = [add, multiply, subtract, divide];
 const toolsByName = Object.fromEntries(tool.map((tool) => [tool.name, tool]));
 const llmWithTools = llm.bindTools(tools); //? This is where we give the tools context to the llm
+
+//! Creating Graph with """LangGraph""" -> It wil have multiple node interconnected with each other -> each node is a process that run the llm with the tools and can also have the memory for previous context
+
+//? This are nodes that performs task by user with tools and llm
+async function llmCall(state) {
+    const result = await llmWithTools.invoke([
+        {
+            role: 'system',
+            content: 'You are a helpful assistant tasked with performing arithmetic on a set of inputs'
+        },
+        ...state.messages,
+    ]);
+
+    return {
+        messages: [result]
+    };
+}
+
+async function toolNode(state) {
+    const results = [];
+    const lastMessage = state.messages.at(-1);
+
+    if (lastMessage?.tool_calls?.length) {
+        for (const toolCall of lastMessage.tool_calls) {
+            const tool = toolsByName[toolCall.name];
+            const observation = await tool.invoke(toolCall.args);
+            results.push(
+                new ToolMessages({
+                    content: observation,
+                    tool_call_id: toolCall.id,
+                })
+            )
+        }
+    }
+
+    return { messages: results };
+}
+
+function shouldContinue(state) {
+    const messages = state.messages;
+    const lastMessage = messages.at(-1);
+
+    //? If the LLM makes a tool call, then perform an action
+    if (lastMessage?.tool_calls?.length) {
+        return "Action";
+    }
+
+    //? Otherwise, we stop (reply to the user)
+    return "__end__";
+}
+
+//? Here we creating the graph that connects the nodes we created before with the tools
+const agentBuilder = new StateGraph(MessagesAnnotation)
+    .addNode('llmCall', llmCall)
+    .addNode('tools', toolNode)
+    .addEdge('__start__', "llmCall")
+    .addConditionalEdges(
+        'llmCall',
+        shouldContinue,
+        {
+            "Action": "tools",
+            "__end__": "__end__"
+        }
+    )
+    .addEdge("tools", "llmCall")
+    .compile()
