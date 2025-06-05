4c4af26c20294e9d6b151a5f17baa8970469d339
Tamal <ckbtamaldipnew@gmail.com>
2025-06-05 21:05:27 +0600

    add simple document for vector storing

--- RAG/src/index.ts
+++ RAG/src/index.ts
@@ -1,1 +1,27 @@
-console.log("Hello langGraph");
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import type { DocumentInterface } from "@langchain/core/documents";
+
+//! That is the simple documents for the vector store
+const document: DocumentInterface[] = [
+  {
+    pageContent:
+      "JavaScript is a versatile programming language primarily used for we development",
+    metadata: { id: "1" },
+  },
+  {
+    pageContent:
+      "LangChain is a powerful library for building language model applications.",
+    metadata: { id: "2" },
+  },
+  {
+    pageContent:
+      "Retrieval-Augmented Generation combines retrieval based and generative models",
+    metadata: { id: "3" },
+  },
+  {
+    pageContent:
+      "LangSmith ia tool that aids in the development and debugging of language model applications.",
+    metadata: { id: "1" },
+  },
+];
